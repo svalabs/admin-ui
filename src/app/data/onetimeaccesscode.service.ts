766436d29cf40a10663e0442d1e8e774de320c5b
yosefha <yosef.hagos@sva.de>
2021-11-23 15:17:59 +0100

    Working on the services to generate and retrieve the one time access codes

--- src/app/data/onetimeaccesscode.service.ts
+++ src/app/data/onetimeaccesscode.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ServerResponse } from './serverresponse';
+import { switchMap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { atou } from '../unicode';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OnetimeaccesscodeService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+
+  public get(id: string) {
+    return this.http.get(environment.server + "/a/scheduledevent/" + id)
+      .pipe(
+        switchMap((response: ServerResponse) => {
+          let se = JSON.parse(atou(response.content));
+          se.start_time = new Date(se.start_time);
+          se.end_time = new Date(se.end_time);
+          return of(se);
+        })
+      )
+  }
 }
